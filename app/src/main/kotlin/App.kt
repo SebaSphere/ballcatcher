9a341190e30bac996b5c56109ea8e58ed708f872
sebasphere <sebasphereemails@gmail.com>
2026-02-16 19:46:36 -0500

    feat: lets not skip yeah

--- app/src/main/kotlin/App.kt
+++ app/src/main/kotlin/App.kt
@@ -87,9 +87,9 @@ suspend fun controlStepperTest(pi4j: Context) {
         // from what I understand, it's supposed to pulse steps (thus the high and low)
         repeat(200) { i ->
             pulse.high()
-            delay(1)
+            delay(2)
             pulse.low()
-            delay(1)
+            delay(2)
         }
 
 //        println("Direction CCW")
