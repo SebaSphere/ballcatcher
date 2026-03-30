ad7680afd9e7370ffce5fed1b7ab00c156a4be2f
sebasphere <sebasphereemails@gmail.com>
2026-03-30 16:28:46 -0400

    chore: don't start home

--- app/src/main/kotlin/App.kt
+++ app/src/main/kotlin/App.kt
@@ -31,26 +31,26 @@ suspend fun main() {
             }
         }
 
-        launch {
-            yawController.motorControl.calibrateHome()
-            delay(5000)
-            println("Moving to 0.5 (midpoint)...")
-            yawController.moveToPosition(0.5f)
-            println("Arrived at midpoint.")
-            delay(1000)
-            yawController.moveToPosition(0.0f)
-            println("Arrived at 0.0.")
-            delay(1000)
-            yawController.moveToPosition(0.75f)
-            println("Arrived at 0.75.")
-            delay(1000)
-            yawController.moveToPosition(0.25f)
-            println("Arrived at 0.25.")
-            delay(1000)
-            yawController.moveToPosition(1f)
-            println("Arrived at 1.")
-
-        }
+//        launch {
+//            yawController.motorControl.calibrateHome()
+//            delay(5000)
+//            println("Moving to 0.5 (midpoint)...")
+//            yawController.moveToPosition(0.5f)
+//            println("Arrived at midpoint.")
+//            delay(1000)
+//            yawController.moveToPosition(0.0f)
+//            println("Arrived at 0.0.")
+//            delay(1000)
+//            yawController.moveToPosition(0.75f)
+//            println("Arrived at 0.75.")
+//            delay(1000)
+//            yawController.moveToPosition(0.25f)
+//            println("Arrived at 0.25.")
+//            delay(1000)
+//            yawController.moveToPosition(1f)
+//            println("Arrived at 1.")
+//
+//        }
 
 
 
